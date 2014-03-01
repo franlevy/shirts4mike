f753c6933c4c37368015f22481a096b9baed2e7e
Fran Levy <flevy@post.harvard.edu>
2014-03-01 17:52:54 -0600

    First commit. This is a PHP website project created through a course at TeamTreehouse.com.

---
+++ shirts.php
@@ -0,0 +1,13 @@
+<?php 
+    $pageTitle = "Mike's Full Catalog of Shirts";
+    $section = "shirts";
+    include("includes/header.php");
+?>
+
+    <div class="section page">
+        <h1>Mike&rsquo;s Full Catalog of Shirts</h1>
+    </div>
+
+<?php
+    include('includes/footer.php');
+?>
